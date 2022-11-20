1dd9f08a45e54d0ea6e94c52948fce2d0d9f0a20
Felipe Gerber <felipegerber@outlook.com>
2022-11-20 15:14:32 -0300

    Finalização do projeto Password Generator

--- Trybe-Archives/asynchronous/FrontEnd/src/main.js
+++ Trybe-Archives/asynchronous/FrontEnd/src/main.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+//console.log('Hello World');
+
+import { nanoid } from "nanoid";
+import './style.css'; //adiciona o estilo no JS
+
+let password = null;
+
+const passwordButtonEl = document.querySelector('button');
+const displayPassEl = document.querySelector('h2');
+
+passwordButtonEl.addEventListener('click', ()=>{
+   password = nanoid();
+
+   displayPassEl.innerHTML = password;
+});
