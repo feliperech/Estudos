fc536868aa6dad20a0515187e882c6e5036271ed
Felipe Gerber <felipegerber@outlook.com>
2022-12-01 11:52:13 -0300

    Finalização da parte teórica

--- Trybe-Archives/asynchronous/FrontEnd/Lecture3/FancyButtons/fancybuttons/src/App.js
+++ Trybe-Archives/asynchronous/FrontEnd/Lecture3/FancyButtons/fancybuttons/src/App.js
@@ -2,29 +2,52 @@ import React from 'react';
 import './App.css';
 
 
-function HandleClickOne(){
-  console.clear();
-  console.log('Você clicou no primeiro botão!');
-}
 
-function HandleClickTwo(){
-  console.clear();
-  console.log('Você clicou no segundo botão!');
-}
-
-function HandleClickThree(){
-  console.clear();
-  console.log('Você clicou no terceiro botão!');
-}
 
 class App extends React.Component {
+  HandleClickOne(){
+    console.clear();
+    console.log('Você clicou no primeiro botão!');
+    this.setState((estadoAnterior, _props) => ({ // underline apresenta que esse parametro foi passado mas não sera usado.
+      numeroCliques1: estadoAnterior.numeroCliques1 +1
+    }));
+  }
+  
+  HandleClickTwo(){
+    console.clear();
+    console.log('Você clicou no segundo botão!');
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliques2: estadoAnterior.numeroCliques2 +1
+    }));
+  }
+  
+  HandleClickThree(){
+    console.clear();
+    console.log('Você clicou no terceiro botão!');
+    this.setState((estadoAnterior, _props) => ({
+      numeroCliques3 : estadoAnterior.numeroCliques3 +1
+    }));
+  }
+
+  constructor () { // chama o constructor para dentro do app extends, permitindo que se tenha controle do this
+    super(); // Importando o super, tem-se acesso ao this.
+    this.HandleClickOne = this.HandleClickOne.bind(this); // permite-se utilizar o this com a func
+    this.HandleClickTwo = this.HandleClickTwo.bind(this);
+    this.HandleClickThree = this.HandleClickThree.bind(this); 
+
+    this.state = { // acessa o state para atualizar o estado.
+      numeroCliques1 : 0, 
+      numeroCliques2 : 0,
+      numeroCliques3 : 0,
+    }
+  }
   render () {
     
     return (
       <>
-        <button onClick = {HandleClickOne}>Primeiro Botão</button>
-        <button onClick = {HandleClickTwo}>Segundo Botão</button>
-        <button onClick = {HandleClickThree}>Terceiro Botão</button>
+        <button onClick = {this.HandleClickOne}>{this.state.numeroCliques1}</button>
+        <button onClick = {this.HandleClickTwo}>{this.state.numeroCliques2}</button>
+        <button onClick = {this.HandleClickThree}>{this.state.numeroCliques3}</button>
       </>
     );
   }
